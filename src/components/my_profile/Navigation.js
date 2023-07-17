e2d85e3a54dede2ee0e69eaf5a4c920c68787091
abdelfetah18 <nstart0319@gmail.com>
2023-07-17 19:57:30 +0100

    Add Icons to the navigation Sidebar

--- src/components/my_profile/Navigation.js
+++ src/components/my_profile/Navigation.js
@@ -1,21 +1,25 @@
-import { FaSignOutAlt, FaUsers } from "react-icons/fa";
+import { FaCog, FaCompass, FaPlus, FaSignOutAlt, FaUsers } from "react-icons/fa";
 
 const navigations = [
     {
         label: "CREATE ROOM",
         path: "/room/create",
+        Icon: FaPlus
     },
     {
         label: "MY ROOMS",
         path: "/profile",
+        Icon: FaUsers
     },
     {
         label: "EXPLORE",
         path: "/explore",
+        Icon: FaCompass
     },
     {
         label: "SETTINGS",
         path: "/settings",
+        Icon: FaCog
     }
 ];
 
@@ -26,11 +30,11 @@ export default function Navigation({ user, selected_label }){
                 <img className="w-16 h-16 rounded-full" src="/profile_1_1.png" />
                 <div className="text-lg text-gray-200 font-semibold mt-2">{user.username}</div>
             </div>
-            <div className="w-full flex flex-col">
+            <div className="w-full flex flex-col items-center">
                 {
                     navigations.map((nav_item, index) => {
                         return (
-                            <NavItem key={index} label={nav_item.label} path={nav_item.path} selected={nav_item.label == selected_label} />
+                            <NavItem key={index} Icon={nav_item.Icon} label={nav_item.label} path={nav_item.path} selected={nav_item.label == selected_label} />
                         )
                     })
                 }
@@ -43,10 +47,13 @@ export default function Navigation({ user, selected_label }){
     )
 }
 
-const NavItem = ({ selected, path, label }) => {
-    const default_style = "hover:bg-sky-800 cursor-pointer px-10 py-2 text-gray-100 font-semibold text-sm text-center rounded-r-lg duration-300";
+const NavItem = ({ selected, path, label, Icon }) => {
+    const default_style = "w-full flex flex-row items-center px-12 text-start hover:bg-sky-800 cursor-pointer py-2 text-gray-100 rounded-r-lg duration-300";
     
     return(
-        <a href={path} className={default_style + (selected ? " bg-sky-700" : "")}>{label}</a>
+        <a href={path} className={default_style + (selected ? " bg-sky-700" : "")}>
+            <Icon />
+            <div className="font-semibold text-sm text-center ml-2">{label}</div>
+        </a>
     )
 }
