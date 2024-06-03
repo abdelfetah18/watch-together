badd8fe83a6bdfb6ea40484f5651f5a221802998
abdelfetah18 <nstart0319@gmail.com>
2024-06-03 19:56:41 +0100

    Fix user profile_image property

--- src/database/client.ts
+++ src/database/client.ts
@@ -13,7 +13,7 @@ const client: SanityClient = createClient({
 const USER_PROPS = '{ _id,username,"profile_image":profile_image.asset-> }';
 const USER_PASSWORD_PROPS = '{ _id, username, password, "profile_image": profile_image.asset->url }';
 const ROOM_PROPS = '{ _id,"profile_image": profile_image.asset->,categories[]->,admin->, creator->, name, privacy, password, description, bio, "total_members": count(*[_type=="member" && @.room._ref==^._id]) }';
-const MESSAGE_PROPS = '{"user":user->{ _id,username,"profile_image":@.profile_image.asset->url },message,type,_createdAt } | order(@._createdAt asc)';
+const MESSAGE_PROPS = '{"user":user->{ _id,username,"profile_image":@.profile_image.asset-> },message,type,_createdAt } | order(@._createdAt asc)';
 const MEMBER_PROPS = `{_id, user->${USER_PROPS}, room->${ROOM_PROPS}, permissions }`;
 const ROOM_CATEGORY_PROPS = '{ _id, name }';
 
