e9a6e04d642ab9e3342823cbd7e752268da51448
abdelfetah18 <nstart0319@gmail.com>
2026-01-19 21:57:02 +0100

    Disable the ability to change theme for now

--- src/components/landing_page/Header.tsx
+++ src/components/landing_page/Header.tsx
@@ -1,9 +1,7 @@
-import ThemeContext from "@/contexts/ThemeContext";
-import { useContext } from "react";
-import { FaMoon, FaSignInAlt, FaSun } from "react-icons/fa";
+import { FaSignInAlt } from "react-icons/fa";
 
 export default function Header() {
-    const { theme, toggleTheme } = useContext(ThemeContext);
+    // const { theme, toggleTheme } = useContext(ThemeContext);
 
     function scrollTo(index: number): () => void {
         return () => {
@@ -20,10 +18,10 @@ export default function Header() {
                 <img className="h-16" src="/logo.png" />
             </div>
             <div className="flex flex-row items-center gap-8">
-                <div onClick={toggleTheme} className={styles.navItem}>
+                {/* <div onClick={toggleTheme} className={styles.navItem}>
                     {theme == 'light' && <FaMoon />}
                     {theme == 'dark' && <FaSun />}
-                </div>
+                </div> */}
                 <div onClick={scrollTo(1)} className={styles.navItem}>Features</div>
                 {/* <div onClick={scrollTo(2)} className={styles.navItem}>FAQ</div> */}
                 <a href="/user/sign_in" className="flex justify-center items-center gap-2 bg-primary-color text-white w-40 py-2 rounded-lg active:scale-110 duration-300">
